4ccdfe36f238ac7a522ff5eaf5fb9f68ca9a912c
vturchanikov <113292747+vturchanikov@users.noreply.github.com>
2023-02-02 13:47:44 +0400

    Initial commit

---
+++ PhoneShop/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneShop.Models;
+
+public class Product
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { set; get; }
+    public int Price { get; set; }
+
+    public long CategoryId { get; set; }
+    public Category Category { get; set; }
+
+}
