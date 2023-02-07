d1a416a8e2cdbbd6663427c4dd13e2863a47e250
vturchanikov <113292747+vturchanikov@users.noreply.github.com>
2023-02-07 12:50:46 +0400

    Revert "Change"
    This reverts commit e952e170138c17b746025332987633b033c2be0e.

--- PhoneShop/Controllers/ProductController.cs
+++ PhoneShop/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PhoneShop.Interfaces;
 using PhoneShop.Models;
+using PhoneShop.ViewModels;
 
 namespace PhoneShop.Controllers
 {
@@ -8,11 +9,13 @@ namespace PhoneShop.Controllers
     {
         private readonly ICategoryRepository _categoryRepository;
         private readonly IProductRepository _productRepository;
+        private readonly IPhotoService _photoService;
 
-        public ProductController(ICategoryRepository categoryRepository, IProductRepository productRepository)
+        public ProductController(ICategoryRepository categoryRepository, IProductRepository productRepository, IPhotoService photoService)
         {
             _categoryRepository = categoryRepository;
             _productRepository = productRepository;
+            _photoService = photoService;
         }
 
         public ViewResult Index() =>
@@ -39,14 +42,41 @@ namespace PhoneShop.Controllers
         }
 
         [HttpPost]
-        public IActionResult UpdateProduct(Product product)
+        public async Task<IActionResult> UpdateProduct(UpdateProductViewModel productVM)
         {
-            if (product.Id == 0)
+            var result = await _photoService.AddPhotoAsync(productVM.Image);
+            if (productVM.Id == 0)
             {
+                
+
+                var product = new Product
+                {
+                    Name = productVM.Name,
+                    Description = productVM.Description,
+                    ShortDescrition = productVM.ShortDescrition,
+                    PurchasePrice = productVM.PurchasePrice,
+                    RetailPrice = productVM.RetailPrice,
+                    Availability = productVM.Availability,
+                    ImgaeLink = result.Url.ToString(),
+                    CategoryId = productVM.CategoryId
+                };
+
                 _productRepository.AddProduct(product);
             }
             else
             {
+                var product = new Product
+                {
+                    Name = productVM.Name,
+                    Description = productVM.Description,
+                    ShortDescrition = productVM.ShortDescrition,
+                    PurchasePrice = productVM.PurchasePrice,
+                    RetailPrice = productVM.RetailPrice,
+                    Availability = productVM.Availability,
+                    ImgaeLink = result.Url.ToString(),
+                    CategoryId = productVM.CategoryId
+                };
+
                 _productRepository.UpdateProduct(product);
             }
 
