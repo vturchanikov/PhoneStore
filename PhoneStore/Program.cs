2f7111311163772414df93f843afc425d1aa6088
vturchanikov <113292747+vturchanikov@users.noreply.github.com>
2023-03-06 19:46:11 +0400

    FinalChanges

---
+++ PhoneStore/Program.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PhoneStore.Data;
+using PhoneStore.Helpers;
+using PhoneStore.Interfaces;
+using PhoneStore.Repositories;
+using PhoneStore.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+string identityConnectionString = builder.Configuration.GetConnectionString("IdentityConnection");
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseSqlServer(identityConnectionString));
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireLowercase = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(10);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    //options.SignIn.RequireConfirmedAccount = true;
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityDbContext>().AddDefaultTokenProviders();
+
+
+builder.Services.AddDistributedSqlServerCache(options =>
+{
+    options.ConnectionString = connectionString;
+    options.SchemaName = "dbo";
+    options.TableName = "SessionData";
+});
+
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "SportStore.Session";
+    options.IdleTimeout = System.TimeSpan.FromHours(48);
+    options.Cookie.HttpOnly = false;
+});
+
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+builder.Services.AddTransient<ISendGridEmail, SendGridEmail>();
+builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration.GetSection("SendGrid"));
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseStatusCodePages();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseSession();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "pages",
+    pattern: "{controller=Store}/{action=Index}/{page=options.CurrentPage}"
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Store}/{action=Index}/{id?}");
+
+app.Run();
---
+++ PhoneStore/Repositories/OrderRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneStore.Data;
+using PhoneStore.Interfaces;
+using PhoneStore.Models;
+using PhoneStore.Models.Pages;
+
+namespace PhoneStore.Repositories
+{
+    public class OrderRepository : Repository, IOrderRepository
+    {
+        public OrderRepository(DataContext context) : base(context) { }
+
+        public IEnumerable<Order> Orders => _context.Orders
+            .Include(o => o.Lines).ThenInclude(l => l.Product);
+
+        public PageList<Order> GetOrders(QueryOptions options)
+        {
+            return new PageList<Order>(_context.Orders.Include(o => o.Lines).ThenInclude(l => l.Product), options);
+        }
+
+        public IEnumerable<Order> GetOrdersByUserName(string userName)
+        {
+
+            return _context.Orders.Where(o => o.UserName == userName).Include(o => o.Lines);
+        }
+        
+        public Order GetOrder(long id)
+        {
+            return _context.Orders
+                .Include(o => o.Lines).FirstOrDefault(o => o.Id == id);
+        }
+
+        public void AddOrder(Order order)
+        {
+            _context.Orders.Add(order);
+
+            Save();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            _context.Orders.Update(order);
+
+            Save();
+        }
+
+        public void DeleteOrder(Order order)
+        {
+            _context.Orders.Remove(order);
+
+            Save();
+        }
+    }
+}
